a4789d880c0b66283eca3f0c8409076e6c1791f8
Regan Anderson <llisandur@outlook.com>
2025-05-29 11:21:22 -0500

    initial

---
+++ default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+  nerd-font-patcher,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "eorzean-typeface-nerdfont";
+  version = "1.051";
+
+  src = fetchurl {
+    url = "https://github.com/karaipsum/Eorzean-Typeface/releases/download/v${version}/AugmentedNeo-Eorzean-Regular.otf";
+    hash = "sha256-SeIJ6uQjCeV0IwJoSt557nIHBk1l6J+N7ef7LzS2ugs=";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [
+    nerd-font-patcher
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    nerd-font-patcher --adjust-line-height --progressbars --complete $src
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 AugmentedNeoEorzeanNerdFont-Regular.otf -t $out/share/fonts/opentype
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Nerdfont patch of Augmented Neo-Eorzean";
+    homepage = "https://github.com/karaipsum/Eorzean-Typeface";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
